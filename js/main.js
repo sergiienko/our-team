2ef3b4e97732b53e181cac710dc8492d9a0d68fa
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-10-19 21:13:29 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-;(function() {
+;(function($) {
 
-})();
+	$(function() {
+		var $slider = $(".ba-team-slider");
+
+		$slider.slick({
+			dots: true,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			prevArrow: "<svg class='slick-prev'><use xlink:href='#icon-angle-left'></use></svg>",
+			nextArrow: "<svg class='slick-next'><use xlink:href='#icon-angle-right'></use></svg>"
+		});
+
+	});
+
+})(jQuery);
