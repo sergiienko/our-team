8a9d1950f7e5f796d10ea32d7ab789f3d6045667
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-10-21 15:14:52 +0300

    Added button and stripes and fixed bugs.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 ;(function($) {
 
 	$(function() {
-		var $slider = $(".ba-team-slider");
+		var $slider = $(".ba-team-slider"),
+			$prev = $(".slick-prev", $slider),
+			$next = $(".slick-next", $slider);
 
 		$slider.slick({
 			dots: true,
-			autoplay: true,
-			autoplaySpeed: 3000,
-			prevArrow: "<svg class='slick-prev'><use xlink:href='#icon-angle-left'></use></svg>",
-			nextArrow: "<svg class='slick-next'><use xlink:href='#icon-angle-right'></use></svg>"
+			prevArrow: $prev,
+			nextArrow: $next,
+			slide: ".ba-crewman__slide"
 		});
 
 	});
